970d249b64eef509b3fe5011d62cf50ee64daaa4
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-08-11 01:59:40 +0300

    Listing Creation, step 3. (Counters)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "@/app/components/inputs/CategoryInput";
 import {FieldValues, useForm} from "react-hook-form";
 import CountrySelect from "@/app/components/inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "@/app/components/inputs/Counter";
 
 enum STEPS {
     CATEGORY,
@@ -49,6 +50,9 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(() => dynamic(() => import('@/app/components/Map'), {
         ssr: false
@@ -108,11 +112,36 @@ const RentModal = () => {
 
     if (step === STEPS.LOCATION) {
         bodyContent = (
-          <div className={'flex flex-col gap-8 '}>
+          <div className={'flex flex-col gap-8'}>
               <Heading title={'Где находится жильё?'} subtitle={'Помогите гостям найти вас!'} />
               <CountrySelect onChange={(value) => setCustomValue('location', value)} value={location} />
               <Map center={location?.latlng} />
           </div>
+        );
+    }
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className={'flex flex-col gap-8'}>
+                <Heading title={'Расскажите самое главное о вашем жилье'} subtitle={'Какие удобства у вас есть?'} />
+                <Counter title={'Гости'}
+                         subtitle={'Сколько гостей разрешено заселять?'}
+                         value={guestCount}
+                         onChange={(value) => setCustomValue('guestCount', value)}
+                />
+                <hr />
+                <Counter title={'Жилые комнаты'}
+                         subtitle={'Сколько комнат в вашем жилье?'}
+                         value={roomCount}
+                         onChange={(value) => setCustomValue('roomCount', value)}
+                />
+                <hr />
+                <Counter title={'Ванные комнаты'}
+                         subtitle={'Сколько санузлов в вашем жилье?'}
+                         value={bathroomCount}
+                         onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
+            </div>
         );
     }
 
