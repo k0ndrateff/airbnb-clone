68cad928bde57bc90b2e90311d7c5d7380a46255
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-08-25 02:37:52 +0300

    fix: dynamic rendering build error

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const Home = async ({ searchParams }: HomeProps) => {
         <Container>
           <div className={'pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5' +
               ' 2xl:grid-cols-6 gap-8'}>
-              {listings.map((listing) => {
+              {listings.map((listing: any) => {
                   return (
                       <ListingCard key={listing.id} data={listing} currentUser={currentUser} />
                   );
